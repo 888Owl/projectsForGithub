3b32fa90579a0d624c916df22880be697c505a0e
Sam Swift <smshulk@aol.com>
2020-11-10 12:50:58 +0800

    First Vue project added
    Used vue to create a basic crypto comparison of xmr btc eth boy were the results shocking

---
+++ crypto-compare/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CryptoChart />
+  </div>
+</template>
+
+<script>
+import CryptoChart from './components/CryptoChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CryptoChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ crypto-compare/src/components/CryptoChart.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <a href="https://www.coingecko.com/en/coins/bitcoin">Bitcoin on Coingecko</a><br/>
+    <a href="https://www.coingecko.com/en/coins/ethereum">Ethereum on Coingecko</a><br/>
+    <a href="https://www.coingecko.com/en/coins/monero">Monero on Coingecko</a><br/>
+    <br/><br/><br/>
+    <div class="chart-wrapper">
+      <apexchart
+        v-if='this.series[0].data.length === 27 && this.series[1].data.length === 27 && this.series[2].data.length === 27'
+        width="800"
+        type="line"
+        :options="options"
+        :series="series"
+      >
+      </apexchart>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CryptoChart",
+  data: () => ({
+   options: {
+      chart: {
+        id: "vuechart-example",
+      },
+      dataLabels: {
+              enabled: true,
+            },
+      xaxis: {
+        categories: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+        ],
+      },
+      title: {
+        text: "Recent Price Changes",
+        align: "center",
+        style: {
+          fontSize: "20px",
+        },
+      },
+      colors: ["#00897b", 'red', 'blue' ],
+    },
+    series: [
+      {
+        name: "BTC",
+        data: [],
+      },
+      {
+        name: "ETH",
+        data: [],
+      },
+      {
+        name: "XMR",
+        data: [],
+      },
+    ],
+  }),
+  // created() {
+  //   console.log(this.series)
+  // }
+  created() {
+    let vv = this;
+    // Simple GET request using fetch
+    fetch("https://api.coinranking.com/v1/public/coins")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          for (let i = 0; i < data.data.coins.length; i++) {
+            if (data.data.coins[i].symbol === "XMR") {
+              vv.series[2].data = data.data.coins[i].history;
+            }
+            if (data.data.coins[i].symbol === "BTC") {
+              vv.series[0].data = data.data.coins[i].history;
+            }
+            if (data.data.coins[i].symbol === "ETH") {
+              vv.series[1].data = data.data.coins[i].history;
+            }
+          }
+        }
+      });
+  },
+};
+</script>
+
+<style scoped>
+div.chart-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
