d958f94e44143db49ff96ac3b7afbd0a52d6b28d
Sam Swift <smshulk@aol.com>
2020-11-10 16:19:34 +0800

    beginning redesigns
    Added a collection of images and beginning to update styles of projects

--- wikipedia-searchbar/root.js
+++ wikipedia-searchbar/root.js
@@ -13,8 +13,13 @@ $(document).ready(function () {
             async: true,
             success: function (data) {
                 $('#reciever').empty();
+                if (data[1].length < 1) {
+                    console.log(123)
+                    errMsg()
+                }
+
                 for (var i = 0; i < data[1].length; i++) {
-                    $('#reciever').append("<li><a href= " + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
+                    $('#reciever').append("<li><a class='portala' href= " + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
                 };
                 $("#search-bar").val(" ");
             },
@@ -29,3 +34,22 @@ $(document).ready(function () {
         });
     });
 });
+
+const errMsg = () => {
+    console.log(321)
+    let errELem = document.getElementById('errELem')
+    let shadow = document.getElementById('shadow')
+
+    errELem.style.display = 'flex'
+    console.log(errELem.style)
+    shadow.style.display = 'flex'
+    sleep(3000).then(() => {
+        console.log('1wdf`  wev')
+        errELem.style.display = 'none'
+        shadow.style.display = 'none'
+    })
+}
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
